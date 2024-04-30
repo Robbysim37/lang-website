3e05567a0e591c992d3595ee279ff4de4ee16641
Robby Lewis <Robbysim37@gmail.com>
2024-04-30 11:43:47 -0400

    angular grid is unituitive

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-videos',
   templateUrl: './videos.component.html',
-  styleUrl: './videos.component.css'
+  styleUrl: './videos.component.scss'
 })
 export class VideosComponent {
 
