2880266720351c838e9bbb803a0c231cedfdd359
Robby Lewis <Robbysim37@gmail.com>
2024-05-04 16:45:26 -0400

    made a grid for all the people in langsite

---
+++ src/app/people/person-grid-card/person-grid-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-grid-card',
+  templateUrl: './person-grid-card.component.html',
+  styleUrl: './person-grid-card.component.scss'
+})
+export class PersonGridCardComponent {
+
+}
